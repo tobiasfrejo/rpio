41dfca9f868ccade5696e5f05973f008a5ea3cab
BVANACKER <bert.vanacker@uantwerpen.be>
2024-10-23 10:03:50 +0200

    -- removed numbering in folder structure

---
+++ examples/HelloWorld/rpio_commands_placeholder.py
@@ -0,0 +1,17 @@
+from rpio.transformations.transformations import swc2code_py,message2code_py
+from examples.HelloWorld.Design.HelloWorld_AADLIL import *
+
+#-------------------------------------------------------------------------------------------------------
+# !! THESE COMMANDS NORMALLY ARE CALLED FROM THE RPIO CLI, FOR TESTING PURPOSE, A PYTHON FILE IS USED !!
+#-------------------------------------------------------------------------------------------------------
+
+# 1. GENERATE AADL FROM ROBOARCH (NOT IMPLEMENTED YET)
+
+# 2. GENERATE AADL INTERMEDIATE LANGUAGE FROM AADL (NOT IMPLEMENTED YET)
+
+# 2. LOAD THE AADL INTERMEDUATE LANGUAGE (MOCKUP)
+design = HelloWorldDesign()
+
+# 3. GENERATE CUSTOM MESSAGES FROM AADL INTERMEDIATE LANGUAGE
+
+# 4. GENERATE SWC CODE FROM AADL INTERMEDIATE LANGUAGE
