ad0c66d7e766759150fb68e8f73a1d96a3026b01
BVANACKER <bert.vanacker@uantwerpen.be>
2024-10-24 09:04:21 +0200

    -- requirement.txt and docker file added

--- rpio/transformations/transformations.py
+++ rpio/transformations/transformations.py
@@ -10,6 +10,56 @@ from os import mkdir
 from os.path import exists, dirname, join
 import jinja2
 
+#-----------------------------------------------------------------------------------
+#-----------------------------------AUXILIARY---------------------------------------
+#-----------------------------------------------------------------------------------
+def _AddRequirementsFile(file="requirements.txt", path=None):
+    """Function to add a requirement.txt to the provided path."""
+
+    # --- open file ---
+    f = open(path + "/" + file, "a")
+
+    # --- custom file content ---
+    f.write("rpio==0.1.0\n")
+    f.write("jsonpickle==3.3.0\n")
+    f.write("paho-mqtt==2.1.0\n")
+    f.write("PyYAML==6.0.2\n")
+
+
+    # --- close file ---
+    f.close()
+
+def _AddDockerFile(file="Dockerfile",cmpName="", path=None):
+    """Function to add a requirement.txt to the provided path."""
+
+    # --- open file ---
+    f = open(path + "/" + file, "a")
+
+    # --- custom file content ---
+    f.write("FROM python:3.10\n")
+    f.write("ENV PYTHONUNBUFFERED 1\n")
+    f.write("\n")
+    f.write("ADD . c:/src/app\n")
+    f.write("WORKDIR c:/src/app\n")
+    f.write("ENV PYTHONPATH c:/src/app:$PYTHONPATH\n")
+    f.write("\n")
+    f.write("COPY requirements.txt ./\n")
+    f.write("RUN pip3 install --no-cache-dir -r requirements.txt\n")
+    f.write("\n")
+    f.write("COPY config.yaml ./\n")
+    f.write("COPY messages.py ./\n")
+    f.write("COPY . .\n")
+    f.write("\n")
+    f.write('CMD ["python3", "'+cmpName+'.py"]\n')
+    f.write("\n")
+
+    # --- close file ---
+    f.close()
+
+
+#-----------------------------------------------------------------------------------
+#--------------------------------TRANSFORMATIONS------------------------------------
+#-----------------------------------------------------------------------------------
 def swc2code_py(system=None,path="output/generated"):
 
     if not exists(path):
@@ -46,6 +96,14 @@ def swc2code_py(system=None,path="output/generated"):
         with open(join(swcFolder, "messages.py"), 'w') as f:
             f.write(templateMessages.render(messages=system.messages))
 
+        # 4. Add requirements.txt
+        _AddRequirementsFile(path=swcFolder)
+
+        # 5. Add Docker file
+        _AddDockerFile(cmpName=process.name, path=swcFolder)
+
+
+
 def message2code_py(system=None,path="output/generated/messages"):
 
     if not exists(path):
@@ -61,4 +119,8 @@ def message2code_py(system=None,path="output/generated/messages"):
     # Extract all processes from AADL system model
     with open(join(path, "messages.py"), 'w') as f:
         f.write(template.render(messages=system.messages))
+
+
+
+
 
