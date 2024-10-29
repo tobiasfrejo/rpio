43d444944af46a54fcf8002133579d8850aed596
BVANACKER <bert.vanacker@uantwerpen.be>
2024-10-29 21:54:07 +0100

    -- submodules automodule test

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.join(os.path.abspath('.'),'rpio','pyLauncher') )
+sys.path.insert(1, os.path.join(os.path.abspath('.'),'rpio','transformations') )
+
 print(sys.path)
 
 
