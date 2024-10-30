6480abfe04e59a7554e676c9c1dc6368cfcef21b
BVANACKER <bert.vanacker@uantwerpen.be>
2024-10-30 22:43:36 +0100

    -- build version updated

--- rpio/__init__.py
+++ rpio/__init__.py
@@ -7,7 +7,7 @@
 # * permission of Bert Van Acker
 # *************************************************************************
 
-VERSION = (0, 1, "0")
+VERSION = (0, 2, "0")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "robosapiensio"
