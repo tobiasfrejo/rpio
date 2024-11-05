945c520192427a5eaa0570506029340a880d98cc
BVANACKER <bert.vanacker@uantwerpen.be>
2024-11-05 12:23:22 +0100

    -- preparation for release v0.3.2 (CLI in release)

--- rpio/__init__.py
+++ rpio/__init__.py
@@ -7,7 +7,7 @@
 # * permission of Bert Van Acker
 # *************************************************************************
 
-VERSION = (0, 3, "2")
+VERSION = (0, 3, "2-b")
 __version__ = ".".join([str(s) for s in VERSION])
 
 __title__ = "robosapiensio"
