0e9812241267c27d23fc9e814febd707f6435a27
BVANACKER <bert.vanacker@uantwerpen.be>
2024-12-10 10:55:57 +0100

    -- workflow tasks added

---
+++ examples/HelloWorld/Workflows/AADL2CODE.py
@@ -0,0 +1,14 @@
+# **********************************************************************************
+# * Copyright (C) 2024-present Bert Van Acker (B.MKR) <bert.vanacker@uantwerpen.be>
+# *
+# * This file is part of the roboarch R&D project.
+# *
+# * RAP R&D concepts can not be copied and/or distributed without the express
+# * permission of Bert Van Acker
+# **********************************************************************************
+from rpio.workflow.tasks import *
+
+# 1. generate messages
+t_generate_messages()
+
+t_update_robosapiensIO_ini()
--- rpio/package/manager.py
+++ rpio/package/manager.py
@@ -232,6 +232,11 @@ class PackageManager(object):
             f.write("[RoboSAPIENSIO]\n")
             f.write('name = '+name+'\n')
             f.write('description = " Add project description"\n')
+            f.write('\n')
+            f.write("[PACKAGE]\n")
+            f.write("[PACKAGE]\n")
+            f.write("name = "+name+"\n")
+            f.write("prefix = " + path + "\n")
 
 
         if "run.py" in file:
